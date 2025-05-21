cf4a263790e07daf36ce6c85594007b271b9a14c
schmeow <amuchhal112@gmail.com>
2025-05-21 08:12:31 -0400

    tabs... o-o

--- scripts.js
+++ scripts.js
@@ -9,8 +9,18 @@ const windows = document.querySelectorAll(".window");
 function onMouseDrag(event, element) {
     let leftValue = parseInt(window.getComputedStyle(element).left);
     let topValue = parseInt(window.getComputedStyle(element).top);
-    element.style.left = `${leftValue + event.movementX}px`;
-    element.style.top = `${topValue + event.movementY}px`;
+    var moveX = (leftValue + event.movementX < 0)? 0 : ((leftValue + event.movementX + element.offsetWidth > innerWidth)? innerWidth - element.offsetWidth : leftValue + event.movementX);
+    element.style.left = `${moveX}px`;
+
+    var moveY = (topValue + event.movementY < 0)? 0 : topValue + event.movementY;
+    if (moveY + element.offsetHeight > innerHeight - innerHeight*(4.85/100)) {
+        moveY = (innerHeight - innerHeight*(4.85/100)) - element.offsetHeight;
+    }
+    //var moveY = (topValue + event.movementY < 0)? 0 : topValue + event.movementY;
+    // var moveY = (topValue + event.movementY > innerHeight)? innerHeight : topValue + event.movementY;
+    element.style.top = `${moveY}px`;
+
+
 }
 
 windows.forEach((element) => {
